0df6dc1dbd9a6d313896a696fede77ef6db82008
Grapeka <chotwilaiwan@gmail.com>
2023-01-15 17:01:56 +0700

    `feat` : remove log innerWidth

--- src/components/TopSection/TopSection.js
+++ src/components/TopSection/TopSection.js
@@ -1,27 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Hero from "../Hero/Hero";
-import MobileHero from "../MobileHero/MobileHero";
-import Section from "../../UI/Section/Section";
+import Hero from '../Hero/Hero';
+import MobileHero from '../MobileHero/MobileHero';
+import Section from '../../UI/Section/Section';
 const TopSection = (props) => {
   const [screenStatus, setScreenStatus] = useState(
-    window.innerWidth < 1024 ? "mobile" : "desktop"
+    window.innerWidth < 1024 ? 'mobile' : 'desktop'
   );
   const reportWindowSize = (e) => {
-    console.log(e.target.innerWidth);
     if (e.target.innerWidth < 1024) {
-      setScreenStatus("mobile");
+      setScreenStatus('mobile');
     } else {
-      setScreenStatus("desktop");
+      setScreenStatus('desktop');
     }
   };
   useEffect(() => {
-    window.addEventListener("resize", reportWindowSize);
+    window.addEventListener('resize', reportWindowSize);
   }, []);
 
   return (
     <Section>
-      {screenStatus === "desktop" ? (
+      {screenStatus === 'desktop' ? (
         <Hero username={props.username} setUsername={props.setUsername} />
       ) : (
         <MobileHero />
